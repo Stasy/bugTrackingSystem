500b06b27a1e1dc902da15fa1ebfd1403fc77821
Stacy Pi <stacy.pi@ya.ru>
2015-05-12 21:44:27 +0500

    логика переходов кроме loginPage

---
+++ Scripts/mainScripts.js
@@ -0,0 +1,15 @@
+function GoToEditIssuePage(){
+    window.location.pathname = "/bugTrackingSystem/static/EditIssuePage.html";
+}
+
+function GoToEditProjectPage(){
+    window.location.pathname = "/bugTrackingSystem/static/EditProjectPage.html";
+}
+
+function GoToLoginAndRegistrationPage(){
+    window.location.pathname = "/bugTrackingSystem/static/LoginAndRegistrationPage.html";
+}
+
+function GoToMainPage(){
+    window.location.pathname = "/bugTrackingSystem/static/MainPage.html";
+}
